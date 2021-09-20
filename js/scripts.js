9d808ab5501b8b49a8f8e1e0ff6b766732ad3d98
reubie <r.nguyo27@gmail.com>
2021-09-20 16:09:58 +0300

    adjust home page

--- js/scripts.js
+++ js/scripts.js
@@ -51,7 +51,65 @@ function Address (streetAddress, city, state, zipcode) {
 	this.deliveryAddress = (streetAddress + "  " + city + ", " + state + "  " + zipcode);
   }
 
-  $ (document).ready(function() {
+  //User Interface Logic
+$(document).ready(function(event) {
+
+  $("#order-btn").click(function() {
+    $("#order-content").show();
+    $("#landing-content").hide();
+    $("#delivery-option").text("Order Now");
+  });
+  $("#pickup-btn").click(function() {
+    $("#order-content").show();
+    $("#landing-content").hide();
+    $("#delivery-option").text("PICKUP BY CUSTOMER");
+  });
+  $("#delivery-btn").click(function() {
+    $("#address").show();
+    $("#pickup-btn,#delivery-btn,#landing-tagline").hide();
+  });
+  $("form#address-form").submit(function(event) {
+    event.preventDefault();
+    var streetAddress = $("input#street-add").val();
+    var city = $("input#city-add").val();
+    var state = $("select#state-select").val();
+    var zipcode = $("input#zip-add").val();
+    var newAddress = new Address(streetAddress, city, state, zipcode)
+    $("#order-content").show();
+    $("#landing-content").hide();
+    $("#delivery-option").text("DELIVER TO: " + newAddress.deliveryAddress);
+  });
+  $("form#custom-pizza").submit(function(event) {
+    event.preventDefault();
+    var customSize = $("select#size").val();
+    var sauce = $("select#sauce").val();
+    var cheese = $("select#cheese").val();
+    var veggie1 = $("select#veggie1").val();
+    var veggie2 = $("select#veggie2").val();
+    var meat = $("select#meat").val();
+    var pizzaDetails = (customSize + " - " + sauce + ", " + cheese + ", " + veggie1 + ", " + veggie2 + ", " + meat);
+    var newPizzaOrder = new Order(customSize, cheese);
+    newPizzaOrder.pizzaCost();
+    totalPriceArray.push(newPizzaOrder.pizzaPrice);
+    $("#pizza-details-dropdown").show();
+    $("#final-cost").text(newPizzaOrder.finalCost());
+    $("#pizza-details").append("<ul><li>" + pizzaDetails + "</li></ul>");
+    $("#size, #sauce, #cheese, #veggie1, #veggie2, #meat").val("");
+  });
+  $("#pizza-details-dropdown").click(function() {
+    $("#pizza-details").toggle();
+  });
+
+  ///Checkout Btn
+
+  $("#order-proceed-btn").click(function() {
+	  alert(" Thank you for your order, It will be processed shortly.");
+    location.reload();
+  });
+});
+
+
+$ (document).ready(function() {
 	// calcBtn click function
 	$("#calcBtn").click(function() {	
 	
@@ -207,121 +265,59 @@ function Address (streetAddress, city, state, zipcode) {
 			
 	});
 	// end of calcBtn click function
-
-  //User Interface Logic
-$(document).ready(function(event) {
-
-  $("#order-btn").click(function() {
-    $("#order-content").show();
-    $("#landing-content").hide();
-    $("#delivery-option").text("Order Now");
-  });
-  $("#pickup-btn").click(function() {
-    $("#order-content").show();
-    $("#landing-content").hide();
-    $("#delivery-option").text("PICKUP BY CUSTOMER");
-  });
-  $("#delivery-btn").click(function() {
-    $("#address").show();
-    $("#pickup-btn,#delivery-btn,#landing-tagline").hide();
-  });
-  $("form#address-form").submit(function(event) {
-    event.preventDefault();
-    var streetAddress = $("input#street-add").val();
-    var city = $("input#city-add").val();
-    var state = $("select#state-select").val();
-    var zipcode = $("input#zip-add").val();
-    var newAddress = new Address(streetAddress, city, state, zipcode)
-    $("#order-content").show();
-    $("#landing-content").hide();
-    $("#delivery-option").text("DELIVER TO: " + newAddress.deliveryAddress);
-  });
-  $("form#custom-pizza").submit(function(event) {
-    event.preventDefault();
-    var customSize = $("select#size").val();
-    var sauce = $("select#sauce").val();
-    var cheese = $("select#cheese").val();
-    var veggie1 = $("select#veggie1").val();
-    var veggie2 = $("select#veggie2").val();
-    var meat = $("select#meat").val();
-    var pizzaDetails = (customSize + " - " + sauce + ", " + cheese + ", " + veggie1 + ", " + veggie2 + ", " + meat);
-    var newPizzaOrder = new Order(customSize, cheese);
-    newPizzaOrder.pizzaCost();
-    totalPriceArray.push(newPizzaOrder.pizzaPrice);
-    $("#pizza-details-dropdown").show();
-    $("#final-cost").text(newPizzaOrder.finalCost());
-    $("#pizza-details").append("<ul><li>" + pizzaDetails + "</li></ul>");
-    $("#size, #sauce, #cheese, #veggie1, #veggie2, #meat").val("");
-  });
-  $("#pizza-details-dropdown").click(function() {
-    $("#pizza-details").toggle();
-  });
-
-  	
+	
 	// used to test whether a selection was made
-	function isEmpty(obj){	
-		if (Array.isArray(obj)) {
-			if (obj.length == 0) { return true; }
-			else { return false; }
-		} else {
-			if (obj==null || obj == "") { return true; }
-			else if (obj.length === 0) { return true; }
-			return false;
+		function isEmpty(obj){	
+			if (Array.isArray(obj)) {
+				if (obj.length == 0) { return true; }
+				else { return false; }
+			} else {
+				if (obj==null || obj == "") { return true; }
+				else if (obj.length === 0) { return true; }
+				return false;
+			};
 		};
+	
+	// display error message
+	function addErrorMsg(errorMsg) {
+		$("#orderErrorMsg").text(errorMsg);
+		$("#orderErrorMsg").show();			
 	};
 
-// display error message
-function addErrorMsg(errorMsg) {
-	$("#orderErrorMsg").text(errorMsg);
-	$("#orderErrorMsg").show();			
-};
-
-// hide error message section and reset color on headers
-function clearErrorMsg() {
-	$("#orderErrorMsg").hide();
-	document.getElementById("pizza-size-header").style.color = "#231f20";
-	document.getElementById("crustBtn").style.color = "#fff";
-	document.getElementById("sauceBtn").style.color = "#fff";
-	document.getElementById("cheeseBtn").style.color = "#fff";
-	document.getElementById("meatBtn").style.color = "#fff";
-	document.getElementById("veggieBtn").style.color = "#fff";
-};	
-
+	// hide error message section and reset color on headers
+	function clearErrorMsg() {
+		$("#orderErrorMsg").hide();
+		document.getElementById("pizza-size-header").style.color = "#231f20";
+		document.getElementById("crustBtn").style.color = "#fff";
+		document.getElementById("sauceBtn").style.color = "#fff";
+		document.getElementById("cheeseBtn").style.color = "#fff";
+		document.getElementById("meatBtn").style.color = "#fff";
+		document.getElementById("veggieBtn").style.color = "#fff";
+	};	
+	
 });	
 
 $(".testimonials-slider").bxSlider({
-auto:true,
-controls: true,
-nextText : '',
-prevText : '',
-pause: 5000,
-speed: 500,
-slideMargin : 30,
-pager : true,
-pagerCustom: '.testimonials-slider-pager-one, .testimonials-slider-pager-two, .testimonials-slider-pager-three'
+    auto:true,
+    controls: true,
+    nextText : '',
+    prevText : '',
+    pause: 5000,
+    speed: 500,
+    slideMargin : 30,
+    pager : true,
+    pagerCustom: '.testimonials-slider-pager-one, .testimonials-slider-pager-two, .testimonials-slider-pager-three'
 });
 
 $(document).ready(function() {
-$('.fa-search').on("click", function() {
-	$('.header-search .header-input').css("display","inline-block");
-	$(".close-icon").css("display","inline-block");
-	$(".search-icon").css("display","none");
+    $('.fa-search').on("click", function() {
+        $('.header-search .header-input').css("display","inline-block");
+        $(".close-icon").css("display","inline-block");
+        $(".search-icon").css("display","none");
+    });
+    $('.close-icon').on("click", function() {
+        $('.header-search .header-input').css("display","none");
+        $(".close-icon").css("display","none");
+        $(".search-icon").css("display","inline-block");
+    });
 });
-$('.close-icon').on("click", function() {
-	$('.header-search .header-input').css("display","none");
-	$(".close-icon").css("display","none");
-	$(".search-icon").css("display","inline-block");
-});
-});
-
-  ///Checkout Btn
-
-  $("#order-proceed-btn").click(function() {
-	  alert(" Thank you for your order, It will be processed shortly.");
-    location.reload();
-  });
-});
-
-
-
-
